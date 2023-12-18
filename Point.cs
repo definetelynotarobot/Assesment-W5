84c0634703a663f9824bbb7ebbdd6d97eb35d531
Mehmet Emin Eraslan <mehmetemineraslan7@gmail.com>
2023-12-19 02:17:37 +0300

    Add project files.

---
+++ Point.cs
@@ -0,0 +1,21 @@
+﻿namespace MethodsPractice
+{
+    public class Point
+    {
+        public int X = 0;
+        public int Y = 0;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(int a0, int a1, int a2, int a3)
+        {
+            this.X = X + a0 + a2;
+            this.Y = Y + a1 + a3;
+            Console.WriteLine("Yeni konumunuz: ({0}, {1})", X, Y);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MethodsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Haritanın enini ve boyunu giriniz:");
+            var haritaX = int.Parse(Console.ReadLine());
+            var haritaY = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Başlangıç noktası: (0, 0)");
+
+            Point point = new Point(0, 0);
+
+            Console.WriteLine("Hareket koordinatlarını giriniz:");
+            var a0 = int.Parse(Console.ReadLine());
+            var a1 = int.Parse(Console.ReadLine());
+            var a2 = int.Parse(Console.ReadLine());
+            var a3 = int.Parse(Console.ReadLine());
+
+            point.Move(a0, a1, a2, a3);
+
+            Console.WriteLine("Yeni konumunuz: ({0}, {1})", point.X, point.Y);
+
+            Console.WriteLine("İnsan(1) olarak mı Uzaylı(2) olarak mı ilerlemek istersiniz?");
+            var secim = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
